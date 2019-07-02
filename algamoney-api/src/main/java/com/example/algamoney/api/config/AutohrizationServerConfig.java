b9737a70c4f3a8e2358a48837810d98526e7e31b
Danniel <dannielgd@gmail.com>
2019-07-02 15:43:13 -0300

    Aula 6.5 - Usando JWT

--- algamoney-api/src/main/java/com/example/algamoney/api/config/AutohrizationServerConfig.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/AutohrizationServerConfig.java
@@ -9,7 +9,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -35,12 +36,21 @@ public class AutohrizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
 			.tokenStore(tokenStore()) //armazena o token em algum lugar para recuperar depois.
+			.accessTokenConverter(accessTokenConverter())
 			.authenticationManager(authenticationManager);
 	}
 	
 	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("algaworks"); // senha que valida o token
+		return accessTokenConverter;
+	}
+
+	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		//return new InMemoryTokenStore(); //Com JWT não se armazena mais em memória
+		return new JwtTokenStore(accessTokenConverter());
 	}
 
 }
--- algamoney-api/src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
@@ -19,7 +19,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("admin").password("{noop}admin").roles("ROLE");
+			.withUser("admin").password("admin").roles("ROLE");
 	}
 	
 	/**
