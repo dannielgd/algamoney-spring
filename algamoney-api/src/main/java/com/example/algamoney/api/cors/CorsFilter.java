c9e891525ffcd1256bdb52fcc185db400f2ef967
Danniel <dannielgd@gmail.com>
2019-07-05 19:58:53 -0300

    Preparado_para_Producao

--- algamoney-api/src/main/java/com/example/algamoney/api/cors/CorsFilter.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/cors/CorsFilter.java
@@ -24,7 +24,7 @@ public class CorsFilter implements Filter {
 	@Autowired
 	private AlgamoneyApiProperty algamoneyApiProperty;
 	
-	private String originPermitida = algamoneyApiProperty.getOriginPermitida(); // Configurar para diferentes ambientes.
+	private String origin;  // Configurar para diferentes ambientes.
 	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
@@ -32,10 +32,10 @@ public class CorsFilter implements Filter {
 		HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) resp;
 		
-		response.setHeader("Access-Control-Allow-Origin", originPermitida);
+		response.setHeader("Access-Control-Allow-Origin", algamoneyApiProperty.getOriginPermitida());
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		
-		if("OPTIONS".equals(request.getMethod()) && originPermitida.equals(request.getHeader("Origin"))) {
+		if("OPTIONS".equals(request.getMethod()) && algamoneyApiProperty.getOriginPermitida().equals(request.getHeader("Origin"))) {
 			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
 			response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
 			response.setHeader("Access-Control-Max-Age", "3600");
